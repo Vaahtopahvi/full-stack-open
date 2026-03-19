4e7b3d71d471ba3b66f4206f95caa9867e51bf9b
riku <kermakahvi@hotmail.com>
2026-03-19 16:57:45 +0200

    update code to 1c

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+// const App = () => {
+//   const course = {
+//     name: "Half Stack application development",
+//     parts: [
+//       {
+//         name: "Fundamentals of React",
+//         exercises: 10,
+//       },
+//       {
+//         name: "Using props to pass data",
+//         exercises: 7,
+//       },
+//       {
+//         name: "State of a component",
+//         exercises: 14,
+//       },
+//     ],
+//   };
 
-  return (
-    <div>
-      <h1>{course.name}</h1>
-      <p>{course.parts[0].name}</p>
-      <p>{course.parts[0].exercises}</p>
-      <p>{course.parts[1].name}</p>
-      <p>{course.parts[1].exercises}</p>
-      <p>{course.parts[2].name}</p>
-      <p>{course.parts[2].exercises}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>{course.name}</h1>
+//       <p>{course.parts[0].name}</p>
+//       <p>{course.parts[0].exercises}</p>
+//       <p>{course.parts[1].name}</p>
+//       <p>{course.parts[1].exercises}</p>
+//       <p>{course.parts[2].name}</p>
+//       <p>{course.parts[2].exercises}</p>
+//     </div>
+//   );
+// };
+
+// const arto = {
+//   name: 'Arto Hellas',
+//   age: 35,
+//   education: 'PhD',
+//   greet: function() {
+//     console.log('hello, my name is ' + this.name)
+//   },
+//   doAddition: function(a, b) {
+//     console.log(a + b)
+//   },
+// }
+
+// arto.doAddition(1, 4)        // 5 is printed
+
+// const referenceToAddition = arto.doAddition
+// referenceToAddition(10, 15)   // 25 is printed
 
 //--------------------------------------------------------------vaihtoehtonen ratkasu
 // const course = "Half Stack application development";
@@ -63,5 +80,65 @@ const App = () => {
 //     );
 //   });
 // };
+
+//----------------- DESTRUKTUROINTI & USE STATE ------------------------------
+
+// const Hello = ({ mini, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age;
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {mini}, you are {age} years old
+//       </p>
+//       <p>So you were probably born {bornYear()}</p>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const nimi = "Pekka";
+//   const ika = 10;
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello mini="Maya" age={26 + 10} />
+//       <Hello mini={nimi} age={ika} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import Button from "./components/Button";
+import Display from "./components/Display";
+import { useState } from "react";
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+  const setToZero = () => {
+    console.log("Setting value to zero", counter);
+    setCounter(0);
+  };
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
+    </div>
+  );
+};
 
 export default App;
