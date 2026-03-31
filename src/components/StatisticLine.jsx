55982d7665813c09ac902a8b930b94a714820821
riku <kermakahvi@hotmail.com>
2026-03-31 15:56:40 +0300

    added smaller component to display stats and for clearer reusability + testing

---
+++ src/components/StatisticLine.jsx
@@ -0,0 +1,8 @@
+const StatisticLine = ({ text, value }) => (
+  //   console.log("rendering statistic line with text:", text, "and value:", value),
+  <p>
+    {text} {value}
+  </p>
+);
+
+export default StatisticLine;
