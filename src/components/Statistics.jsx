f8512caf8a27afd7a10a8cd65296008ffeb048b1
riku <kermakahvi@hotmail.com>
2026-03-31 15:55:27 +0300

    re-do the code to use StatisticLine component and clean it completely

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,35 +1,32 @@
+import StatisticLine from "./StatisticLine";
+
+// paljon cleanimpi koodi...
 const Stats = (props) => {
-  return (
-    <div>
-      <p>
-        {props.text}
-        {props.text && props.amount}
-      </p>
+  // jos tekstiä lähetetään tänne, käytä sitä ja amounttia tässä.
+  // lähetä StatisticLineen text ja amount, mikä palauttaa p:n, jossa on teksti ja amount.
+  if (props.text) {
+    return <StatisticLine text={props.text} value={props.amount} />;
+  }
 
-      {/* jos avg prop on määritetty, niin renderöi p elementti, joka näyttää avg tekstin ja sum/total laskukaavan tuloksen, muuten ei mitään */}
-      {props.avg && (
-        <p>
-          {props.avg}
-          {props.sum !== undefined && props.total !== undefined
-            ? (props.sum / props.total).toFixed(2)
-            : 0}
-        </p>
-      )}
+  // jos avg lähetetään tänne, laske keskiarvo avgValue muuttujaan, muuten näytä 0.
+  // lähetä StatisticLineen "average ", joka on saatu propsina ja avgValue, joka on laskettu edellisessä vaiheessa.
+  if (props.avg) {
+    const avgValue =
+      props.sum !== undefined && props.total !== undefined
+        ? (props.sum / props.total).toFixed(2)
+        : 0;
+    return <StatisticLine text={props.avg} value={avgValue} />;
+  }
 
-      {/* jos avg prop, amount prop, total prop on määritetty, niin renderöi p elementti, joka näyttää avg tekstin ja sum/total laskukaavan tuloksen, muuten ei mitään */}
-      {props.good &&
-        props.amount !== undefined &&
-        props.total !== undefined && (
-          <p>
-            {props.good}
-            {props.total > 0
-              ? ((props.amount / props.total) * 100).toFixed(2)
-              : 0}
-            %
-          </p>
-        )}
-    </div>
-  );
+  // jos good lähetetään tänne, laske goodPercentage muuttujaan goodin prosenttiosuus kokonaisäänistä, muuten näytä null.
+  // lähetä StatisticLineen "good% ", joka on saatu propsina ja goodPercentage, joka on laskettu edellisessä vaiheessa.
+  // helppo nakki.
+  if (props.good) {
+    const goodPercentage =
+      props.total > 0 ? ((props.amount / props.total) * 100).toFixed(2) : 0;
+    return <StatisticLine text={props.good} value={goodPercentage} />;
+  }
+  return null;
 };
 
 export default Stats;
