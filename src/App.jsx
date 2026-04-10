a041c65c98d2467525fa094486bd4e924963c92e
riku <kermakahvi@hotmail.com>
2026-04-10 17:49:11 +0300

    update code with latest changes. done exercises 2.1 - 2.3

--- src/App.jsx
+++ src/App.jsx
@@ -252,63 +252,105 @@
       <Statistics avg={"average "} sum={sum} total={total} />
       <Statistics good={"good% "} amount={good} total={total} /> */
 
-import Button from "./components/Button";
-import { useState } from "react";
-import Statistics from "./components/Statistics";
+// import Button from "./components/Button";
+// import { useState } from "react";
+// import Statistics from "./components/Statistics";
+
+// const App = () => {
+//   // tallenna napit omaan tilaansa
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+//   const [average, setAverage] = useState([]);
+//   const total = good + neutral + bad;
+//   const sum = average.reduce((a, b) => a + b, 0);
+
+//   const handleGoodClick = () => {
+//     setGood(good + 1);
+//     setAverage(average.concat(+1));
+//   };
+
+//   const handleNeutralClick = () => {
+//     setNeutral(neutral + 1);
+//     setAverage(average.concat(0));
+//   };
+
+//   const handleBadClick = () => {
+//     setBad(bad + 1);
+//     setAverage(average.concat(-1));
+//   };
+
+//   // console.log(sum);
+//   // console.log(average);
+//   // console.log("good:", good, "total:", total);
+//   console.log(good);
+
+//   //concat ja join todennäköisesti
+
+//   return (
+//     <div>
+//       <h1>give feedback</h1>
+
+//       <Button onClick={handleGoodClick} text="good" />
+//       <Button onClick={handleNeutralClick} text="neutral" />
+//       <Button onClick={handleBadClick} text="bad" />
+//       <h2>statistics</h2>
+
+//       {/* good, neutral, bad, all, average, good% */}
+//       {total === 0 ? (
+//         <p>No feedback given</p>
+//       ) : (
+//         <div>
+//           <Statistics text={"good "} amount={good} />
+//           <Statistics text={"neutral "} amount={neutral} />
+//           <Statistics text={"bad "} amount={bad} />
+//           <Statistics text={"total "} amount={total} />
+//           <Statistics avg={"average "} sum={sum} total={total} />
+//           <Statistics good={"good% "} amount={good} total={total} />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ----------------------------- Tehtävä 2 ---------------------------
+
+import Note from "./components/Note";
+import Course from "./components/Course";
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [average, setAverage] = useState([]);
-  const total = good + neutral + bad;
-  const sum = average.reduce((a, b) => a + b, 0);
-
-  const handleGoodClick = () => {
-    setGood(good + 1);
-    setAverage(average.concat(+1));
+  const courseVar = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "ReduX",
+        exercises: 11,
+        id: 4,
+      },
+    ],
   };
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-    setAverage(average.concat(0));
-  };
-
-  const handleBadClick = () => {
-    setBad(bad + 1);
-    setAverage(average.concat(-1));
-  };
-
-  // console.log(sum);
-  // console.log(average);
-  // console.log("good:", good, "total:", total);
-  console.log(good);
-
-  //concat ja join todennäköisesti
 
   return (
     <div>
-      <h1>give feedback</h1>
-
-      <Button onClick={handleGoodClick} text="good" />
-      <Button onClick={handleNeutralClick} text="neutral" />
-      <Button onClick={handleBadClick} text="bad" />
-      <h2>statistics</h2>
-
-      {/* good, neutral, bad, all, average, good% */}
-      {total === 0 ? (
-        <p>No feedback given</p>
-      ) : (
-        <div>
-          <Statistics text={"good "} amount={good} />
-          <Statistics text={"neutral "} amount={neutral} />
-          <Statistics text={"bad "} amount={bad} />
-          <Statistics text={"total "} amount={total} />
-          <Statistics avg={"average "} sum={sum} total={total} />
-          <Statistics good={"good% "} amount={good} total={total} />
-        </div>
-      )}
+      <Course kurssi={courseVar} />
     </div>
   );
 };
