940ffde396a412e3a5e0a325fae598a51b3cf702
riku <kermakahvi@hotmail.com>
2026-04-22 17:01:07 +0300

    update the code and complete exercise 2.4

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Course({ kurssi }) {
-  //   console.log(paska);
+  // console.log("ensin", kurssi);
 
-  //   const total = paska.parts.reduce((s, p) => {
+  //   const total = kurssi.parts.reduce((s, p) => {
   //     console.log("what is happening", s, p);
   //     return someMagicHere;
   //   });
-  const result = kurssi.parts.map((part) => part.name);
-  console.log(result);
+  // const result = kurssi.parts.map((part) => part.name);
+  // console.log(result);
+  console.log(kurssi[0].name);
   return (
     <div>
       {/* <h1>{kurssi.name}</h1>
@@ -25,20 +26,54 @@ function Course({ kurssi }) {
         total of exercises:{" "}
         {kurssi.parts.reduce((sum, part) => sum + part.exercises, 0)}
       </h3> */}
-      <h1>{kurssi.name}</h1>
-      {kurssi.parts.map((part) => (
-        <div key={part.id}>
-          <p>
-            {part.name} {part.exercises}
-          </p>
+      {/* <h1>{kurssi.name}</h1> */}
+      {kurssi.map((course) => (
+        <div key={course.id}>
+          <h1>{course.name}</h1>
+          {course.parts.map((part) => (
+            <p key={part.id}>
+              {part.name} {part.exercises}
+            </p>
+          ))}
+          <h3>
+            total of exercises:{" "}
+            {course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+          </h3>
         </div>
       ))}
-      <h3>
+      {/* <h3>
         total of exercises:{" "}
         {kurssi.parts.reduce((sum, part) => sum + part.exercises, 0)}
-      </h3>
+      </h3> */}
     </div>
   );
 }
 
 export default Course;
+
+// const courseVar = {
+// name: "Half Stack application development",
+// id: 1,
+// parts: [
+// {
+// name: "Fundamentals of React",
+// exercises: 10,
+// id: 1,
+// },
+// {
+// name: "Using props to pass data",
+// exercises: 7,
+// id: 2,
+// },
+// {
+// name: "State of a component",
+// exercises: 14,
+// id: 3,
+// },
+// {
+// name: "ReduX",
+// exercises: 11,
+// id: 4,
+// },
+// ],
+// };
