a9e4fc7754755f91f205bbf94bd8329c93925e6f
riku <kermakahvi@hotmail.com>
2026-05-09 21:06:43 +0300

    update PersonForm to communicate with db

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,17 +1,22 @@
+import axios from "axios";
+
 const PersonForm = (props) => {
-  console.log(props);
+  // console.log(props);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (props.persons.some((person) => person.name === props.newName)) {
       alert(`${props.newName} is already added to phonebook`);
     } else {
       const personObject = { name: props.newName, number: props.newNumber };
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          props.setPersons(props.persons.concat(response.data));
+          props.setNewName("");
+          props.setNewNumber("");
+        });
       // console.log("button clicked");
       // console.log(personObject);
-
-      props.setPersons(props.persons.concat(personObject));
-      props.setNewName("");
-      props.setNewNumber("");
     }
   };
 
