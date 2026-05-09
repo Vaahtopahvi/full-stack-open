e884af0ace7681a609cbaaf6d7b1a318e938b81a
riku <kermakahvi@hotmail.com>
2026-05-09 21:05:55 +0300

    create person.js and extract communication to separate module

---
+++ src/services/persons.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
+
+const filter = (name) => {
+  return axios.get(`${baseUrl}/${name}`);
+};
+
+export default {
+  getAll,
+  create,
+  filter,
+};
