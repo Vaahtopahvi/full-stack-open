45be2200aacc480d307f4abc82448e4f914f7fb5
riku <kermakahvi@hotmail.com>
2026-05-12 16:36:02 +0300

    add delete button after each name. exercise 2.14

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,8 +1,13 @@
 const Phonebook = (props) => {
   //   console.log(props.luettelo);
+  // console.log(props);
+  // console.log("moi, katon onks tääl " + props.important);
   return props.luettelo.map((person) => (
     <p key={person.name}>
       {person.name} {person.number}
+      <button onClick={() => props.deletePerson(person.id, person.name)}>
+        delete
+      </button>
     </p>
   ));
 };
