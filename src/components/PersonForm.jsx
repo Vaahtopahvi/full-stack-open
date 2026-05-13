0e22962ca051ce862ae2c41261e7dc50a494b7e5
riku <kermakahvi@hotmail.com>
2026-05-13 16:18:57 +0300

    update code to match exercise 2.15 (modify p. number) and import server calls from services

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,20 +1,41 @@
-import axios from "axios";
+import personService from "../services/persons";
 
 const PersonForm = (props) => {
   // console.log(props);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (props.persons.some((person) => person.name === props.newName)) {
-      alert(`${props.newName} is already added to phonebook`);
+      window.confirm(
+        `${props.newName} is already added to phonebook, replace the old number with a new one?`,
+      );
+      {
+        // APUAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        const personName = props.persons.find(
+          (person) => person.name === props.newName,
+        );
+        console.log("henkilö", personName, props.newNumber);
+        const changedNumber = { ...personName, number: props.newNumber };
+        console.log(changedNumber.id);
+        personService
+          .update(changedNumber.id, changedNumber)
+          .then((response) => {
+            console.log(response);
+            props.setPersons(
+              props.persons.map((person) =>
+                changedNumber.id === person.id ? changedNumber : person,
+              ),
+            );
+            props.setNewName("");
+            props.setNewNumber("");
+          });
+      }
     } else {
       const personObject = { name: props.newName, number: props.newNumber };
-      axios
-        .post("http://localhost:3001/persons", personObject)
-        .then((response) => {
-          props.setPersons(props.persons.concat(response.data));
-          props.setNewName("");
-          props.setNewNumber("");
-        });
+      personService.create(personObject).then((response) => {
+        props.setPersons(props.persons.concat(response.data));
+        props.setNewName("");
+        props.setNewNumber("");
+      });
       // console.log("button clicked");
       // console.log(personObject);
     }
