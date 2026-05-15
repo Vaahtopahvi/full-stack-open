5f1cebc7c8a834b1e4e60762705292f5226efb01
riku <kermakahvi@hotmail.com>
2026-05-15 23:49:01 +0300

    update to display success or error message. complete exercise 2.18

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,9 +1,9 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, type }) => {
   if (message === null) {
     return null;
-  }
-
-  return <div className="success">{message}</div>;
+  } else if (type === "error") {
+    return <div className="error">{message}</div>;
+  } else return <div className="success">{message}</div>;
 };
 
 export default Notification;
