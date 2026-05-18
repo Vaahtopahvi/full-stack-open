ae3d4df8635e7811e584648f2e48610496ead8f0
riku <kermakahvi@hotmail.com>
2026-05-18 17:59:56 +0300

    fix and deconstructure object into bits

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,9 +1,10 @@
-const Notification = ({ message, type }) => {
+const Notification = ({ message }) => {
+  console.log(message);
   if (message === null) {
     return null;
-  } else if (type === "error") {
-    return <div className="error">{message}</div>;
-  } else return <div className="success">{message}</div>;
+  } else if (message.type === "error") {
+    return <div className="error">{message.message}</div>;
+  } else return <div className="success">{message.message}</div>;
 };
 
 export default Notification;
